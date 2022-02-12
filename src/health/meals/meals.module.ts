178f3570a2260a629e482d983aae628481733082
hajnik.maros <hajnik.maros@gmail.com>
2022-02-12 22:52:40 +0100

    Created empty meal component for "meals/new" path.

--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -7,11 +7,15 @@ import { SharedModule } from 'src/auth/shared/shared.module';
 
 // Containers
 import { MealsComponent } from './containers/meals/meals.component';
+import { MealComponent } from './containers/meal/meal.component';
 
-export const ROUTES: Routes = [{ path: '', component: MealsComponent }];
+export const ROUTES: Routes = [
+  { path: '', component: MealsComponent },
+  { path: 'new', component: MealComponent },
+];
 
 @NgModule({
-  declarations: [MealsComponent],
+  declarations: [MealsComponent, MealComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
