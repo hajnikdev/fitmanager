948b3cb43c5f6604c303b90c73efb3bdfd35ae83
Maroš Hajník <hajnik.work@gmail.com>
2022-02-12 10:58:17 +0100

    Updated auth-form component. Created form using reactive form module feature "FormBuilder" created form in component class and attached this form object to the input elements. Applied validators for the formcontrols. Applied ng-content injection to create elements like form header and redirect links to another component login <=> register.

--- src/auth/login/containers/login/login.component.ts
+++ src/auth/login/containers/login/login.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   styleUrls: ['./login.component.scss'],
-  template: ` <div>Login <app-form></app-form></div> `,
+  template: `
+    <div>
+      <app-form>
+        <h1>Login</h1>
+        <a routerLink="/auth/register">Not registered?</a>
+        <button type="submit">Login</button>
+      </app-form>
+    </div>
+  `,
 })
 export class LoginComponent {
   constructor() {}
--- src/auth/register/containers/register/register.component.ts
+++ src/auth/register/containers/register/register.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   styleUrls: ['./register.component.scss'],
-  template: ` <div>Register <app-form></app-form></div> `,
+  template: `
+    <div>
+      <app-form>
+        <h1>Register</h1>
+        <a routerLink="/auth/login">Already have an account?</a>
+        <button type="submit">Register</button>
+      </app-form>
+    </div>
+  `,
 })
 export class RegisterComponent {
   constructor() {}
