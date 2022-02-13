6cb1f03557737c8335b73ac1b4ae2892b6a99c35
hajnik.maros <hajnik.maros@gmail.com>
2022-02-13 12:04:28 +0100

    Fixed meals service.

--- src/health/shared/components/list-item/list-item.component.ts
+++ src/health/shared/components/list-item/list-item.component.ts
@@ -1,13 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-list-item',
   templateUrl: './list-item.component.html',
   styleUrls: ['./list-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListItemComponent implements OnInit {
   @Input() item: any;
+
+  toggled: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  getRoute(item: any) {
+    console.log(item);
+    return [`../meals`, item.$key];
+  }
+
+  toggle() {
+    this.toggled = !this.toggled;
+  }
+
+  removeItem() {}
 }
--- src/health/shared/services/meals/meals.service.ts
+++ src/health/shared/services/meals/meals.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
-import { AngularFireDatabase } from '@angular/fire/database';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  SnapshotAction,
+} from '@angular/fire/database';
 
 import { Store } from 'store';
 import { AuthService, User } from 'src/auth/shared/services/auth/auth.service';
 
 export interface Meal {
-  name: string;
+  name: string | null;
   ingredients: string[];
-  timestamp: number;
-  $key: string;
+  timestamp: number | null;
+  $key: string | null;
   $exists: () => boolean;
 }
 
@@ -20,12 +24,22 @@ export class MealsService {
     .list<Meal[]>(`meals/${this.authService.uid}`)
     .snapshotChanges()
     .pipe(
-      tap((next: any[]) =>
-        this.store.set(
-          'meals',
-          next?.map((value) => value?.payload)
-        )
-      )
+      map((snapshot: SnapshotAction<Meal[]>[]) => {
+        const meals: Meal[] = snapshot.map((value: SnapshotAction<Meal[]>) => {
+          const data = value.payload.val() as Meal | null;
+          return {
+            name: data?.name ?? null,
+            ingredients: data?.ingredients ?? [],
+            timestamp: null,
+            $key: value.key,
+            $exists: value.payload.exists,
+          };
+        });
+
+        console.log(meals);
+        this.store.set('meals', meals);
+        return meals;
+      })
     );
 
   constructor(
